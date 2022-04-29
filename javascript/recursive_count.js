79bf4bdf6ff33f4377b77724703e436a019eabfd
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-29 16:01:43 -0400

    commit tree 31d1366351643b34309e58ffe0f2cb6e737f6be5

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,22 @@
+let count = 0;
 function recursiveCount(num = 0) {
-  // type your code here
+  // redo this code with recursion 
+  // let count = 0;
+  // while (count < 10) {
+  //   console.log(count);
+  //   ++count;
+  // }
+
+  if (count < 10) {
+    console.log(count);
+    recursiveCount(count++);
+  }
+
+  //solution with using num = 0 instead of global veriable - count:
+  // if (num < 10) {
+  //  console.log(num);
+  //  recursiveCount(num + 1);
+  // }
 }
 
 if (require.main === module) {
